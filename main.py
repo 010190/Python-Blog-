f33bbe78bd0bfe33af57ddaf5865a97a6d6dde82
010190 <olczakmichal4@gmail.com>
2025-06-02 13:03:13 +0200

    create Procfile

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms.fields.simple import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 import os
+from dotenv import find_dotenv, load_dotenv
+
+
+dotenv_path = find_dotenv()
+
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DB = os.getenv('DB')
 
 
 # Import your forms from the forms.py
@@ -33,7 +42,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 login_manager = LoginManager()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = SECRET_KEY
 ckeditor = CKEditor(app)
 login_manager.init_app(app)
 Bootstrap5(app)
@@ -66,7 +75,7 @@ class LoginForm(FlaskForm):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -298,4 +307,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
